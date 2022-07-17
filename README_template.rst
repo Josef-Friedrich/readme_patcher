798967ab8b963e4a7819a8171548abffc14ae3fc
Josef Friedrich <josef@friedrich.rocks>
2022-07-17 08:27:29 +0200

    Add some basic code

---
+++ README_template.rst
@@ -0,0 +1,4 @@
+readme_patcher
+==============
+
+Generate README files from templates. Allow input from functions calls and cli output.
--- readme_patcher/__init__.py
+++ readme_patcher/__init__.py
@@ -1,1 +1,22 @@
-#
+import os
+import subprocess
+
+
+def get_path(*path_segments: str) -> str:
+    return os.path.join(os.getcwd(), *path_segments)
+
+
+def read_file(path: str) -> str:
+    file = open(path, 'r')
+    content = file.read()
+    file.close()
+    return content
+
+
+def read_cli_output(command: str) -> str:
+    result = subprocess.run(command, capture_output=True, text=True)
+    return result.stdout
+
+
+def patch():
+    print('patch')
